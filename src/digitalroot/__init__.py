efd23c5d7a10f81af7c80571562ec4588c136632
Ivan <moskvinuwu@gmail.com>
2026-03-29 16:29:51 +0800

    Add __init__.py with version and core.py with function stubs

---
+++ src/digitalroot/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "0.1.0"
+__all__ = [
+    "digital_root",
+    "digital_root_with_steps",
+    "digital_root_fast",
+    "digital_root_batch",
+    "is_digital_root_valid"
+]
---
+++ src/digitalroot/core.py
@@ -0,0 +1,93 @@
+from typing import Union, List, Tuple, Optional
+
+def digital_root(n: Union[int, str]) -> int:
+    """
+    Вычисляет цифровой корень числа.
+    
+    Args:
+        n: число (int) или строка с числом
+        
+    Returns:
+        цифровой корень (число от 0 до 9)
+        
+    Raises:
+        ValueError: если входное значение не является числом
+        
+    Example:
+        >>> digital_root(123)
+        6
+    """
+    # TODO: реализовать
+    pass
+
+
+def digital_root_with_steps(n: Union[int, str]) -> Tuple[int, List[int]]:
+    """
+    Вычисляет цифровой корень и возвращает все промежуточные суммы.
+    
+    Args:
+        n: число или строка с числом
+        
+    Returns:
+        кортеж (цифровой_корень, [список_промежуточных_сумм])
+        
+    Example:
+        >>> digital_root_with_steps(987)
+        (6, [24, 6])
+    """
+    # TODO: реализовать
+    pass
+
+
+def digital_root_fast(n: int) -> int:
+    """
+    Быстрое вычисление цифрового корня по математической формуле O(1).
+    
+    Формула: dr(n) = 1 + (|n| - 1) % 9, при n ≠ 0
+             dr(0) = 0
+    
+    Args:
+        n: целое число
+        
+    Returns:
+        цифровой корень
+        
+    Example:
+        >>> digital_root_fast(123)
+        6
+    """
+    # TODO: реализовать
+    pass
+
+
+def digital_root_batch(numbers: List[Union[int, str]]) -> List[int]:
+    """
+    Вычисляет цифровые корни для списка чисел.
+    
+    Args:
+        numbers: список чисел или строк
+        
+    Returns:
+        список цифровых корней
+        
+    Example:
+        >>> digital_root_batch([123, 987, 0])
+        [6, 6, 0]
+    """
+    # TODO: реализовать
+    pass
+
+
+def is_digital_root_valid(n: int, expected_root: int) -> bool:
+    """
+    Проверяет, является ли expected_root цифровым корнем числа n.
+    
+    Args:
+        n: исходное число
+        expected_root: предполагаемый цифровой корень
+        
+    Returns:
+        True если expected_root является цифровым корнем n
+    """
+    # TODO: реализовать
+    pass
